344f56f71af0251dbe280e1769e3eac405e631bf
VaidehiSavaliya <vaidehisavaliya8@gmail.com>
2025-11-24 15:07:48 +0530

    modified main,tf

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_security_group" "instancesg" {
       from_port = 80
       to_port = 80
       protocol = "tcp"
-      security_groups = [aws_security_group.albsg.id]
+      cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
